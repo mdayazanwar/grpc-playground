683a201e105614f0401150460262f5be44810f2a
Md Ayaz Anwar <anwer.khan14@gmail.com>
2026-05-21 21:49:42 +0530

    Default value impl

--- src/main/java/com/ayaz/sec03/Lec07DefaultValue.java
+++ src/main/java/com/ayaz/sec03/Lec07DefaultValue.java
@@ -1,6 +1,6 @@
 package com.ayaz.sec03;
 
-import com.ayaz.models.sec03.School;
+import com.ayaz.models.sec03.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +13,22 @@ public class Lec07DefaultValue {
         var school = School.newBuilder().build();
 
         log.info("{}", school.getId()); // print 0;
-        log.info("{}", school.getName());
+        log.info("{}", school.getName()); // print nothing. Doesn't throw null pointer exception
+        log.info("{}", school.getAddress().getCity()); // print nothing. Doesn't throw any null pointer exception
+
+        log.info("is default ?: {} ", school.getAddress().equals(Address.getDefaultInstance()));
+
+        log.info("Has address ?: {}", school.hasAddress());
+
+        var lib  = Library.newBuilder().build();
+
+        log.info("book list: {}", lib.getBooksList());
+
+        var dealer = Dealer.newBuilder().build();
+        log.info("inventory map: {}", dealer.getInventoryMap());
+
+        var car = Car.newBuilder().build();
+        log.info("{}", car.getBodyStyle());
+
     }
 }
