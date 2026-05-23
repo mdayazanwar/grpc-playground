d7b12afc11c88620b6d07d206488e7c43ebd3563
Md Ayaz Anwar <anwer.khan14@gmail.com>
2026-05-23 14:57:08 +0530

    Unary api  impl in progress Async call

--- src/main/java/com/ayaz/sec06/GrpcClient.java
+++ src/main/java/com/ayaz/sec06/GrpcClient.java
@@ -1,27 +1,54 @@
 package com.ayaz.sec06;
 
+import com.ayaz.models.sec06.AccountBalance;
 import com.ayaz.models.sec06.BalanceCheckRequest;
 import com.ayaz.models.sec06.BankServiceGrpc;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
 
 public class GrpcClient {
 
     private static  final Logger log  = LoggerFactory.getLogger(GrpcClient.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         var channel = ManagedChannelBuilder.forAddress("localhost", 6565)
                 .usePlaintext()
                 .build();
 
-        var stub = BankServiceGrpc.newBlockingStub(channel);
 
+        //Async Stub Implementation
+        var stub  = BankServiceGrpc.newStub(channel);
+        stub.getAccountBalance(BalanceCheckRequest.newBuilder().setAccountNumber(2).build(), new StreamObserver<AccountBalance>() {
+            @Override
+            public void onNext(AccountBalance accountBalance) {
+                log.info("Balance: {}", accountBalance);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                log.info("Completed");
+            }
+        });
+
+        log.info("Done");
+        Thread.sleep(Duration.ofSeconds(1));
+
+        //Below implementation is for Sync stub
+/*        //var stub = BankServiceGrpc.newBlockingStub(channel);
         var balance = stub.getAccountBalance(BalanceCheckRequest.newBuilder()
                         .setAccountNumber(2)
                 .build());
 
-        log.info("Balance: {}", balance);
+        log.info("Balance: {}", balance);*/
     }
 }
